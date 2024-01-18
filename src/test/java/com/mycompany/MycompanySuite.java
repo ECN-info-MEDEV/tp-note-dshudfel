62340323f88e07bce98819c31ead4cb94523f4cf
hudsonteixeira <hudson.thadeu.teixeira@gmail.com>
2024-01-18 10:52:51 +0100

    [add] tests

---
+++ src/test/java/com/mycompany/MycompanySuite.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4Suite.java to edit this template
+ */
+package com.mycompany;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author hudsonteixeira
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({com.mycompany.tpnotedshudfel.TpnotedshudfelSuite.class})
+public class MycompanySuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+}
